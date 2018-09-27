8622cb734f8b5f06217d1e0c74186fb4442e0d2d
Todd <xxbsh2010@163.com>
2018-09-27 17:21:20 +0800

    fuck

---
+++ KNN_DTW/dtw_series_classification.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 14:26:00 2018
+
+@author: todd
+"""
+import sys
+import collections
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import mode
+from scipy.spatial import distance as dis
+
+class KnnDtw(object):
+    
+    def __init__(self, n_neighbors=5, max_warping_window=10, subsample_step=1):
+        
+        self.n_neighbors = n_neighbors
+        self.max_warping_window = max_warping_window
+        self.subsample_step = subsample_step
+        
+    def fit(self, x, l):
+        
+        self.x = x
+        self.l = l
+    
+    def _dtw_distance(self, ts_a, ts_b, d = lambda x,y: abs(x-y)):
+        
+        # Create cost matrix via broadcasting with large int
+        ts_a, ts_b = np.array(ts_a), np.array(ts_b)
+        M, N = len(ts_a), len(ts_b)
+        cost = sys.maxsize * np.ones((M, N))
+
+        # Initialize the first row and column
+        cost[0, 0] = d(ts_a[0], ts_b[0])
+        for i in range(1, M):
+            cost[i, 0] = cost[i-1, 0] + d(ts_a[i], ts_b[0])
+
+        for j in range(1, N):
+            cost[0, j] = cost[0, j-1] + d(ts_a[0], ts_b[j])
+
+        # Populate rest of cost matrix within window
+        for i in range(1, M):
+            for j in range(max(1, i - self.max_warping_window),
+                            min(N, i + self.max_warping_window)):
+                choices = cost[i - 1, j - 1], cost[i, j-1], cost[i-1, j]
+                cost[i, j] = min(choices) + d(ts_a[i], ts_b[j])
+
+        # Return DTW distance given window 
+        return cost[-1, -1]
+    
+    def _dist_matrix(self, x, y):
+
+        dm_count = 0
+        # Compute condensed distance matrix (upper triangle) of pairwise dtw distances
+        # when x and y are the same array
+        if(np.array_equal(x, y)):
+            x_s = np.shape(x)
+            dm = np.zeros((x_s[0] * (x_s[0] - 1)) // 2, dtype=np.double)
+            
+#            p = ProgressBar(np.shape(dm)[0])
+            
+            for i in range(0, x_s[0] - 1):
+                for j in range(i + 1, x_s[0]):
+                    dm[dm_count] = self._dtw_distance(x[i, ::self.subsample_step],
+                                                      y[j, ::self.subsample_step])
+                    
+                    dm_count += 1
+#                    p.animate(dm_count)
+                    
+            # Convert to squareform
+            dm = dis.squareform(dm)
+            return dm
+        
+        # Compute full distance matrix of dtw distnces between x and y
+        else:
+            x_s = np.shape(x)
+            y_s = np.shape(y)
+            dm = np.zeros((x_s[0], y_s[0])) 
+            dm_size = x_s[0] * y_s[0]
+            
+#            p = ProgressBar(dm_size)
+        
+            for i in range(0, x_s[0]):
+                for j in range(0, y_s[0]):
+                    dm[i, j] = self._dtw_distance(x[i, ::self.subsample_step],
+                                                  y[j, ::self.subsample_step])
+                    # Update progress bar
+                    dm_count += 1
+#                    p.animate(dm_count)
+        
+            return dm
+        
+    def predict(self, x):
+        
+        dm = self._dist_matrix(x, self.x)
+
+        # Identify the k nearest neighbors
+        knn_idx = dm.argsort()[:, :self.n_neighbors]
+
+        # Identify k nearest labels
+        knn_labels = self.l[knn_idx]
+        
+        # Model Label
+        mode_data = mode(knn_labels, axis=1)
+        mode_label = mode_data[0]
+        mode_proba = mode_data[1]/self.n_neighbors
+
+        return mode_label.ravel(), mode_proba.ravel()
